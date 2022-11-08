c6ade2bcf6a2078318a98751e7aa541c92402bf0
Arbaaz Patwari <arbaazpatwari@gmail.com>
2022-11-08 02:19:48 +0000

    Adding project code and files

---
+++ README.md
@@ -0,0 +1,2 @@
+# CO550 Currency Convertor (logbook 2)
+Code for the logbook 2 exercise Currency Convertor (Task 2) using Razor Pages.
---
+++ currency-converter/currency-converter-logbook-2/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Primitives;
+
+namespace currency_converter_logbook_2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public double? euros;
+
+        public void OnGet()
+        {
+            euros = null;
+        }
+
+        public double CalculateOutput(double amount)
+        {
+            return amount * 1.14;
+        }
+
+        public void OnPost()
+        {
+            if (string.IsNullOrEmpty(Request.Form["euros"])) return; // If there is no input value, return
+            this.euros = Math.Round(CalculateOutput(double.Parse(Request.Form["euros"])), 2); // Specifies the euro variable, applies the calculate output function, and rounds it to two decimal places.
+        }
+    }
+}
